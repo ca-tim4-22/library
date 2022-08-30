5166475ec1188142b49f4252c19ecf9e0dcfa222
Nikola Perisic <5dzoni5@gmail.com>
2022-08-30 12:20:10 +0200

    Optimize welcome and login page

--- library-laravel/resources/views/components/styles.blade.php
+++ library-laravel/resources/views/components/styles.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{{asset('style/style.css')}}">
-<link rel="stylesheet" href="{{asset('style/sorting.css')}}">
+<link rel="stylesheet" href="{{asset('css/default/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/sorting/sorting.css')}}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
 {{-- Preload style --}}
