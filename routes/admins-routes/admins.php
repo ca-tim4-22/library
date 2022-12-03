52e7fc4140cd051baacd4fd311bf032ec22af8ce
Nikola <5dzoni5@gmail.com>
2022-12-03 16:13:22 +0100

    Update admins routes

--- routes/admins-routes/admins.php
+++ routes/admins-routes/admins.php
@@ -10,28 +10,27 @@ use App\Http\Controllers\ {
 
 Route::controller(AdminController::class)->group(function() {
 // Administrators
-Route::get('/administratori', [AdminController::class, 'index'])->name('all-admin');
-Route::get('/administrator/{user:username}', [AdminController::class, 'show'])->name('show-admin');
-Route::get('/novi-administrator', [AdminController::class, 'create'])->name('new-admin');
-Route::post('/novi-administrator', [AdminController::class, 'store'])->name('store-admin');
-Route::get('/izmijeni-profil-administratora/{user:username}', [AdminController::class, 'edit'])->name('edit-admin');
-Route::put('/izmijeni-profil-administratora/{id}', [AdminController::class, 'update'])->name('update-admin');
+Route::get('/administratori', 'index')->name('all-admin');
+Route::get('/administrator/{user:username}', 'show')->name('show-admin');
+Route::get('/novi-administrator', 'create')->name('new-admin');
+Route::post('/novi-administrator', 'store')->name('store-admin');
+Route::get('/izmijeni-profil-administratora/{user:username}', 'edit')->name('edit-admin');
+Route::put('/izmijeni-profil-administratora/{id}', 'update')->name('update-admin');
 
 // Delete ownself
-Route::delete('/izbrisi-administratora/{id}', [AdminController::class, 'destroy'])->name('destroy-admin');
-});
+Route::delete('/izbrisi-administratora/{id}', 'destroy')->name('destroy-admin');
 
 // For multiple student delete
-Route::delete('izbrisi-sve/administratore', [AdminController::class, 'deleteMultiple'])->name('delete-all-admins');
+Route::delete('izbrisi-sve/administratore', 'deleteMultiple')->name('delete-all-admins');
 
 // Middleware protection
 Route::middleware('user-delete')->group(function() {
 // Protection for deleting a certain administrator through URI
 Route::get('/administratori/{id}', function ($id) {});
-Route::post('/administratori/{id}', [AdminController::class, 'destroy'])->name('admins.destroy');
+Route::post('/administratori/{id}', 'destroy')->name('admins.destroy');
 });
 
-Route::post('/crop/admin', [AdminController::class, 'crop'])->name('admin.crop');
-
+Route::post('/crop/admin', 'crop')->name('admin.crop');
+});
 
 ?>
