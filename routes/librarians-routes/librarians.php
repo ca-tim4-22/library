e222fff5ae1f85ee748ae610cc313478b8e753b5
Nikola <5dzoni5@gmail.com>
2022-12-03 16:06:18 +0100

    Update librarians routes

--- routes/librarians-routes/librarians.php
+++ routes/librarians-routes/librarians.php
@@ -6,42 +6,40 @@ use Illuminate\Support\Facades\ {
 
 use App\Http\Controllers\ {
     LibrarianController,
-    UserController,
 };
 
 Route::controller(LibrarianController::class)->group(function() {
 // Librarians
-Route::get('/bibliotekari', [LibrarianController::class, 'index'])->name('all-librarian');
-Route::get('/bibliotekar/{user:username}', [LibrarianController::class, 'show'])->name('show-librarian');
+Route::get('/bibliotekari', 'index')->name('all-librarian');
+Route::get('/bibliotekar/{user:username}', 'show')->name('show-librarian');
 
 // Prevent making new librarians if auth user -> librarian
 Route::middleware('librarian-not-librarian')->group(function() {
-Route::get('/novi-bibliotekar', [LibrarianController::class, 'create'])->name('new-librarian');
-Route::post('/novi-bibliotekar', [LibrarianController::class, 'store'])->name('store-librarian');
+Route::get('/novi-bibliotekar', 'create')->name('new-librarian');
+Route::post('/novi-bibliotekar', 'store')->name('store-librarian');
 });
 
-Route::get('/izmijeni-profil-bibliotekara/{user:username}', [LibrarianController::class, 'edit'])->name('edit-librarian');
-Route::put('/izmijeni-profil-bibliotekara/{id}', [LibrarianController::class, 'update'])->name('update-librarian');
+Route::get('/izmijeni-profil-bibliotekara/{user:username}', 'edit')->name('edit-librarian');
+Route::put('/izmijeni-profil-bibliotekara/{id}', 'update')->name('update-librarian');
 
 // Additional features
 
 // Delete ownself
-Route::delete('/izbrisi-bibliotekara/{id}', [LibrarianController::class, 'destroy'])->name('destroy-librarian');
+Route::delete('/izbrisi-bibliotekara/{id}', 'destroy')->name('destroy-librarian');
 
 // For multiple librarian delete
-Route::delete('izbrisi-sve/bibliotekare', [LibrarianController::class, 'deleteMultiple'])->name('delete-all-librarians');
+Route::delete('izbrisi-sve/bibliotekare', 'deleteMultiple')->name('delete-all-librarians');
 
 // Middleware protection
 Route::middleware('user-delete')->group(function() {
 // Protection for deleting a certain librarian through URI
 Route::get('/bibliotekari/{id}', function ($id) {});
-Route::post('/bibliotekari/{id}', [LibrarianController::class, 'destroy'])->name('librarians.destroy');
+Route::post('/bibliotekari/{id}', 'destroy')->name('librarians.destroy');
 });
 
-Route::post('/resetuj-lozinku/{user}', [UserController::class, 'resetPassword'])->name('resetPassword');
-Route::post('/crop/bibliotekar', [LibrarianController::class, 'crop'])->name('librarian.crop');
+Route::post('/resetuj-lozinku/{user}', 'resetPassword')->name('resetPassword');
+Route::post('/crop/bibliotekar', 'crop')->name('librarian.crop');
 
 });
-
 
 ?>
