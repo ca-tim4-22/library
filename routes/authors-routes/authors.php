bb656da1a2ffd412135d7f0ef018a918d688baeb
Nikola <5dzoni5@gmail.com>
2022-12-03 16:14:32 +0100

    Update authors routes

--- routes/authors-routes/authors.php
+++ routes/authors-routes/authors.php
@@ -10,25 +10,26 @@ use App\Http\Controllers\ {
 
 Route::controller(AuthorController::class)->group(function() {
 // Authors
-Route::get('/autori', [AuthorController::class, 'index'])->name('all-author');
-Route::get('/autor/{author:NameSurname}', [AuthorController::class, 'show'])->name('show-author');
-Route::get('/novi-autor', [AuthorController::class, 'create'])->name('new-author');
-Route::post('/novi-autor', [AuthorController::class, 'store'])->name('store-author');
-Route::get('/izmijeni-autora/{author:NameSurname}', [AuthorController::class, 'edit'])->name('edit-author');
-Route::put('/izmijeni-autora/{id}', [AuthorController::class, 'update'])->name('update-author');
-Route::delete('/izbrisi-autora/{id}', [AuthorController::class, 'destroy'])->name('destroy-author');
-});
+Route::get('/autori', 'index')->name('all-author');
+Route::get('/autor/{author:NameSurname}', 'show')->name('show-author');
+Route::get('/novi-autor', 'create')->name('new-author');
+Route::post('/novi-autor', 'store')->name('store-author');
+Route::get('/izmijeni-autora/{author:NameSurname}', 'edit')->name('edit-author');
+Route::put('/izmijeni-autora/{id}', 'update')->name('update-author');
+Route::delete('/izbrisi-autora/{id}', 'destroy')->name('destroy-author');
 
 // Additional features
 
 // For multiple author delete
-Route::delete('izbrisi-sve/autore', [AuthorController::class, 'deleteMultiple'])->name('delete-all-authors');
+Route::delete('izbrisi-sve/autore', 'deleteMultiple')->name('delete-all-authors');
 
 // Middleware protection
 Route::middleware('user-delete')->group(function() {
 // Protection for deleting a certain author through URI
 Route::get('/autori/{id}', function ($id) {});
-Route::post('/autori/{id}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+Route::post('/autori/{id}', 'destroy')->name('authors.destroy');
+});
+
 });
 
 ?>
