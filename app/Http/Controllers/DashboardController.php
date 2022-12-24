063d79e9fd56a5aeb73684b07f8b91b52055c898
Nikola Perisic <5dzoni5@gmail.com>
2022-12-24 01:46:04 +0100

    Enlightn check

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index(Request $request)
     {
