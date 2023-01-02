facaa6f48844d3d242038b10e23c686e863e1e3a
Nikola Perišić <5dzoni5@gmail.com>
2023-01-02 22:42:26 +0100

    Add some tests

--- tests/Feature/SystemTest.php
+++ tests/Feature/SystemTest.php
@@ -4,25 +4,49 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Tests\TestCase;
-use Illuminate\Support\Str;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+//! Change APP_ENV to "testing" in .env file
+//* APP_ENV=testing
 
 class SystemTest extends TestCase
 {
+    // use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_admin_exists()
-    {
-        $response = $this->post('/uloguj-se', [
-            'email' => 'admin@gmail.com',
-            'password' => 'nekalozinka'
-        ]);
- 
-        $this->assertAuthenticated();
+    public function test_test_user_delete_function() {
+        $user = User::create([
+            'name' => 'Test user',
+        ])->delete();
+        
+        $this->assertTrue(true);
     }
- 
+
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/pocetna');
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_user_exists() {
+        $this->assertDatabaseHas('users', [
+            'name' => 'Administrator'
+        ]);
+    }
+
+    public function test_user_types_exists() {
+        $this->assertDatabaseHas('user_types', [
+            'name' => 'student',
+            'name' => 'librarian',
+            'name' => 'administrator',
+        ]);
+    }
+
     public function test_users_can_not_authenticate_with_invalid_password()
     {
         $this->post('/uloguj-se', [
@@ -33,23 +57,9 @@ class SystemTest extends TestCase
         $this->assertGuest();
     }
 
-    public function test_new_users_can_register()
-    {
-        $password = Str::random(10);
-
-        if (!User::where('username', 'username')->exists()) {
-            $response = $this->post('/register', [
-                'name' => 'Test User',
-                'email' => 'test@ets-pg.edu.me',
-                'JMBG' => '8675768256079',
-                'username' => 'username',
-                'password' => $password,
-                'password_confirmation' => $password
-            ]);
-     
-            $this->assertAuthenticated();
-        } else {
-            $this->markTestSkipped('this test has already passed');
-        }
+    public function test_register_page_status() {
+        $response = $this->get('/register');
+        $response->assertDontSee('Sva prava zadržana');
+        $response->assertStatus(404);
     }
 }
