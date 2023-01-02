2604cd143011e6ec8851307a830209c9cb8b098a
Nikola Perišić <5dzoni5@gmail.com>
2023-01-02 13:30:59 +0100

    Change .env.example file

--- config/querydetector.php
+++ config/querydetector.php
@@ -5,7 +5,7 @@ return [
      * Enable or disable the query detection.
      * If this is set to "null", the app.debug config value will be used.
      */
-    'enabled' => env('QUERY_DETECTOR_ENABLED', true),
+    'enabled' => env('QUERY_DETECTOR_ENABLED'),
 
     /*
      * Threshold level for the N+1 query detection. If a relation query will be
